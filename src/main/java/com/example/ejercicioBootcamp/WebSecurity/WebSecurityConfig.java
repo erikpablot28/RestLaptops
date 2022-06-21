bab08288eeff578a707003c5889615556877d9fd
Erik <erikpablot@gmail.com>
2022-06-21 18:43:32 -0500

    prueba

--- src/main/java/com/example/ejercicioBootcamp/WebSecurity/WebSecurityConfig.java
+++ src/main/java/com/example/ejercicioBootcamp/WebSecurity/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.ejercicioBootcamp.WebSecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,11 +19,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
+
                 .antMatchers("/bootstrap").permitAll()
                 .antMatchers("/hola").hasRole("USER")
                 .antMatchers("/api/laptops").hasRole("ADMIN")
                 .antMatchers("/api/laptops/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT).permitAll()
+                .antMatchers(HttpMethod.DELETE).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
